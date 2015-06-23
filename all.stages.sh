874f6bb52d29a6139db6924f8cc86d787500a80b
Soheil <Damangir@users.noreply.github.com>
2015-06-23 19:01:24 +0200

    fix req file check

--- all.stages.sh
+++ all.stages.sh
@@ -5,6 +5,8 @@
 [ ! -f "${BVECS:='not set'}" ] && printf "BVECS (${BVECS}) not found it.\n" >&2 && exit 1
 [ ! -f "${BVALS:='not set'}" ] && printf "BVALS (${BVALS}) not found it.\n" >&2 && exit 1
 
+SCRIPT_DIR=$( cd "$( dirname "${0}" )" && pwd )
+
 export PROCDIR
 export DTIDATA
 export BVECS
