5c8da85a8ffd4a7d61d871d8e1844d91e482c7da
Soheil <Damangir@users.noreply.github.com>
2016-06-17 14:21:44 +0200

    run bedpostc monitor to wait for all threads

--- 3.diffusion_parameters.sh
+++ 3.diffusion_parameters.sh
@@ -40,3 +40,4 @@ mkdir "${DIFPDIR}/dti"
 run_and_log 1.organize.files cp "${CORRDIR}"/data.nii.gz "${CORRDIR}"/nodif_brain_mask.nii.gz "${ORIGDIR}"/bvals "${ORIGDIR}"/bvecs "${DIFPDIR}/dti"
 
 run_and_log 2.bedpostx ${FSLPRE}bedpostx "${DIFPDIR}/dti" --nf=2 --fudge=1  --bi=1000 --model=2
+"${DIFPDIR}/dti.bedpostX/monitor"
