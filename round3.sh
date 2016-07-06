e5dbf245161707c6051af77344d0640a18e067de
Soheil <Damangir@users.noreply.github.com>
2016-07-07 01:15:33 +0200

    Add registration to standard space

--- round3.sh
+++ round3.sh
@@ -5,3 +5,4 @@ SCRIPT_DIR=$( cd "$( dirname "${0}" )" && pwd )/src
 export PROCDIR
 
 ${stage_runner} ${SCRIPT_DIR}/dti.4.tractography.sh
+${stage_runner} ${SCRIPT_DIR}/dti.5.register_tractography.sh
