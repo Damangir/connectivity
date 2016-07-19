84e3659611a9920441c5b252de21d9b1c2740705
Soheil <Damangir@users.noreply.github.com>
2016-07-19 08:41:27 +0200

    log reporting database

--- src/report.0.database.sh
+++ src/report.0.database.sh
@@ -46,23 +46,35 @@ remove_expected_output
 
 read -r a_procdir < <(grep . ${REPORT_LIST})
 
-grep . ${REPORT_ITEMS} | while read -r to_report
-do
-	report_file=$(get_path_for_subject "${a_procdir}" '${REPORTDIR}'"/${to_report}.txt")
-	prev=$(cut -d ' ' -f 1 "${report_file}" | tr '\n' ';')
-	while read -r subj_procdir
+function ensure_consistency {
+	grep . ${REPORT_ITEMS} | while read -r to_report
 	do
-		report_file=$(get_path_for_subject "${subj_procdir}" '${REPORTDIR}'"/${to_report}.txt")
-		current_labels=$(cut -d ' ' -f 1 "${report_file}" | tr '\n' ';')
-		if [ -z "${prev}" ] || [ "${prev}" = "${current_labels}" ]
-		then
-			prev=${current_labels}
-		else
-	  	    printf "Headers in ${report_file} does not match headers in other files.\n" >&2
-	        exit 1
-		fi
-	done< <(grep . ${REPORT_LIST})
-done
+		report_file=$(get_path_for_subject "${a_procdir}" '${REPORTDIR}'"/${to_report}.txt")
+		prev=$(cut -d ' ' -f 1 "${report_file}" | tr '\n' ';')
+		while read -r subj_procdir
+		do
+			report_file=$(get_path_for_subject "${subj_procdir}" '${REPORTDIR}'"/${to_report}.txt")
+			current_labels=$(cut -d ' ' -f 1 "${report_file}" | tr '\n' ';')
+			if [ "${prev}" = "${current_labels}" ]
+			then
+				prev=${current_labels}
+				printf "${current_labels}\n" >> ${QCDIR}/labels.${to_report}.txt
+			else
+		  	    printf "Headers in ${report_file} does not match headers in other files.\n" >&2
+		        exit 1
+			fi
+		done< <(grep . ${REPORT_LIST})
+	done
+}
+
+function extract_values {
+	report_file=$(get_path_for_subject "${1}" '${REPORTDIR}'"/${to_report}.txt")
+	current_values=$(cut -d ' ' -f 2 "${report_file}" | tr '\n' ';')
+	current_id=$(basename "${1}")
+    printf "${current_id};${current_values}\n" >> "${csv_file}"
+}
+
+run_and_log 0.consistency_check ensure_consistency
 
 grep . ${REPORT_ITEMS} | while read -r to_report
 do
@@ -74,9 +86,6 @@ do
 	printf "id;${current_labels}\n" > "${csv_file}"
 	grep . ${REPORT_LIST} | while read -r subj_procdir
 	do
-		report_file=$(get_path_for_subject "${subj_procdir}" '${REPORTDIR}'"/${to_report}.txt")
-		current_values=$(cut -d ' ' -f 2 "${report_file}" | tr '\n' ';')
-		current_id=$(basename "${subj_procdir}")
-	    printf "${current_id};${current_values}\n" >> "${csv_file}"
+		run_and_log 1.extract_values extract_values "${subj_procdir}"
 	done
 done
