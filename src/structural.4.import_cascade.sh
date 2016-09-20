eccf72c8f773ae7c2c69f5d1e9d8c15379cb097b
Soheil <Damangir@users.noreply.github.com>
2016-09-20 11:02:03 +0200

    import WML mask from Cascade report

--- src/structural.4.import_cascade.sh
+++ src/structural.4.import_cascade.sh
@@ -2,14 +2,26 @@
 
 source "$(cd "$(dirname "$0")"&&pwd)/common.sh"
 
+CASCADE_MASK=${CASCADE_MASK:-"wml_mask*.nii.gz"}
+for f in "${CASCADE_DIR}"/${CASCADE_MASK}; do
+	# Cascade_mask should not exist
+	if [ -f "${CURRENT_CASCADE_MASK}" ]
+	then
+		printf "${CASCADE_MASK} matches multiple files in ${CASCADE_DIR}.\n" >&2
+		exit 1
+	fi
+	[ -f "$f" ] && CURRENT_CASCADE_MASK="${f}"
+done
+
 # Expected input files
-depends_on "${CASCADE_DIR}/flair.normalized.nii.gz" "${CASCADE_DIR}/flair.modelfree.pval.nii.gz"
+depends_on "${CASCADE_DIR}/flair.normalized.nii.gz" "${CASCADE_DIR}/flair.modelfree.pval.nii.gz" "${CURRENT_CASCADE_MASK}"
 
 # Expected output files
 set +e
 read -r -d '' REQUIRED_FILES <<- EOM
 	${STR_IMPORTDIR}/flair.normalized.nii.gz
 	${STR_IMPORTDIR}/flair.pval.nii.gz
+	${STR_IMPORTDIR}/flair.wml_mask.nii.gz
 EOM
 set -e
 
@@ -21,6 +33,7 @@ mkdir -p "${STR_IMPORTDIR}"
 
 run_and_log 1.copy_flair cp "${CASCADE_DIR}/flair.normalized.nii.gz" ${STR_IMPORTDIR}/flair.normalized.nii.gz
 run_and_log 2.copy_pval cp "${CASCADE_DIR}/flair.modelfree.pval.nii.gz" ${STR_IMPORTDIR}/flair.pval.nii.gz
+run_and_log 2.copy_mask cp "${CURRENT_CASCADE_MASK}" ${STR_IMPORTDIR}/flair.wml_mask.nii.gz
 
 
 
